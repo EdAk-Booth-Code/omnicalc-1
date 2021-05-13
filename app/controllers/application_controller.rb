494b8727402ae8dc09472a36f41db20a88626fb4
EdAk-Booth <edwardacosta@chicagobooth.edu>
2021-05-13 20:39:05 +0000

    Updated square form Added logic for squaring input numbers and displaying them to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
-    
     render({:template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def calculate_square
+    @num = params.fetch("input_number")
+    @num_to_i = @num.to_i
+    @num_squared = @num_to_i * @num_to_i
+    render({:template => "calculation_templates/square_results.html.erb"})
   end
 end
