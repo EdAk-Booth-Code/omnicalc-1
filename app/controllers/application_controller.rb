fb91518e872cd9ead0254ec4944e64a932002933
EdAk-Booth <edwardacosta@chicagobooth.edu>
2021-05-14 06:09:54 +0000

    Finished square root page logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,19 @@ class ApplicationController < ActionController::Base
 
   def calculate_square
     @num = params.fetch("input_number")
-    @num_to_i = @num.to_i
-    @num_squared = @num_to_i * @num_to_i
+    num_to_i = @num.to_i
+    @num_squared = num_to_i * num_to_i
     render({:template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_square_root_form
+    render({:template => "calculation_templates/square_root_form.html.erb" })
+  end
+
+  def calculate_square_root
+    @num = params.fetch("input_number")
+    num_to_f = @num.to_f
+    @num_square_root = Math.sqrt(num_to_f)
+    render({:template => "calculation_templates/square_root_results.html.erb"})
   end
 end
